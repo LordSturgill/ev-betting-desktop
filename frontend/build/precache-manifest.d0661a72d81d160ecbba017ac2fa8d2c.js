9600670aa71814229a60be5508671a3b175d8e39
Lord Sturgill <jsturgill1273@gmail.com>
2025-05-13 22:15:39 -0400

    Build front end for Electron

---
+++ frontend/build/precache-manifest.d0661a72d81d160ecbba017ac2fa8d2c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac44fce56321988625a4f61b8ff5fba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b10faaec737876195b7d",
+    "url": "/static/js/2.3c58e96f.chunk.js"
+  },
+  {
+    "revision": "2be84d339de34f8d7c0c",
+    "url": "/static/js/main.1796c98e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
